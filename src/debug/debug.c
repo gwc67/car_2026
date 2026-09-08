b2fb462935e14556b5c65acb60096692f00fc7be
gwc <3416218577@qq.com>
2026-09-08 12:33:28 +0800

    dispatch_line -- 意外的正常工作

--- src/debug/debug.c
+++ src/debug/debug.c
@@ -1,98 +1,75 @@
 #include "debug.h"
 #include <stdint.h>
+#include <string.h>
+#include <sys/_intsup.h>
+#include "uart_base.h"
+#include "uarts.h"
 #include "value_to_str.h"
-#include "zephyr/drivers/gpio.h"
+#include "zephyr/init.h"
+// #include "zephyr/drivers/gpio.h"
 #define LINE_BUF_SIZE 128
 
 
 static void  dispatch_line(char* line_pc);
 
-static const struct gpio_dt_spec led_motor_st =GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
+// static const struct gpio_dt_spec led_motor_st =GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
 
-
-void debug_par_check(void)
+//但是现在我没办法保证是一口气就能获得完整字节啊
+int debug_par_check(uint8_t* data,uint32_t len32,void* user_data)
 {
-//     static char s_line_buf_pc[LINE_BUF_SIZE];
-//     uint8_t byte;         
-//     uint8_t lie_pos_uc = 0;
-//     while (!my_ring_buf_is_empty(g_ut2_rx_ring_pst)) {
-//         my_ring_buf_get(g_ut2_rx_ring_pst, &byte, 1);
-//         if (byte == '\n' || byte == '\r') {
-//             if (lie_pos_uc > 0) {
-//                 s_line_buf_pc[lie_pos_uc] = '\0';
-//                 dispatch_line(s_line_buf_pc);
-//                 lie_pos_uc = 0;
-//             }
-//             continue;  // 换行符本身不入缓冲区
-//         }
-//         if (lie_pos_uc < LINE_BUF_SIZE - 1) {
-//             s_line_buf_pc[lie_pos_uc++] = (char)byte;
-//         } else {
-//             lie_pos_uc = 0;
-//         }
-//     }
+    static char s_line_buf[LINE_BUF_SIZE];       
+    uint8_t lie_pos = 0;
+
+    for (int i = 0; i < len32; i++) {
+        if (data[i] == '\n' || data[i] == '\r') {
+            if (lie_pos > 0) {
+                s_line_buf[lie_pos] = '\0';
+                dispatch_line(s_line_buf);
+                lie_pos = 0;
+            }
+            continue;
+        }
+        if (lie_pos < LINE_BUF_SIZE - 1) {
+            s_line_buf[lie_pos++] = (char)data[i];
+        }
+        else {
+            lie_pos = 0;
+        }
+    }
+    
+    // while (!g_ut2_rx_ring_pst)) {
+    //     my_ring_buf_get(g_ut2_rx_ring_pst, &byte, 1);
+    //     if (byte == '\n' || byte == '\r') {
+    //         if (lie_pos_uc > 0) {
+    //             s_line_buf_pc[lie_pos_uc] = '\0';
+    //             dispatch_line(s_line_buf_pc);
+    //             lie_pos_uc = 0;
+    //         }
+    //         continue;  // 换行符本身不入缓冲区
+    //     }
+    //     if (lie_pos_uc < LINE_BUF_SIZE - 1) {
+    //         s_line_buf_pc[lie_pos_uc++] = (char)byte;
+    //     } else {
+    //         lie_pos_uc = 0;
+    //     }
+    // }
 }
 
 
-// static void  dispatch_line(char* line_pc)
-// {
-//     float values_pf[4] = {0};
-//     // int16_t values_ps[4] = {0};
-//     str_to_float(line_pc, values_pf, 4);
+static void  dispatch_line(char* line_pc)
+{
+    float values_pf[4] = {0};
+    // int16_t values_ps[4] = {0};
+    str_to_float(line_pc, values_pf, 4);
 
-//     if (strncmp(line_pc, "spd_kff:", 8) == 0) {
-//         spd_kff = values_pf[0];
-//     }
-//     else if (strncmp(line_pc, "factor_bal_kp:", 14) == 0 ) 
-//     {
-//         bal_kp_factor = values_pf[0];
-//     }
-//     else if (strncmp(line_pc, "factor_bal_ki:", 14)== 0 ) 
-//     {
-//         bal_ki_factor = values_pf[0];
-//     }
-//     else if (strncmp(line_pc, "factor_bal_kd:", 14) == 0) 
-//     {
-//         bal_kd_factor = values_pf[0];
-//     }
-//     else if(strncmp(line_pc, "bal_kp:",6) == 0)
-//     {
-//         BALANCE_KP = values_pf[0];
-//     }
-//     else if (strncmp(line_pc, "bal_ki:", 6) == 0) {
-//         BALANCE_KI = values_pf[0];
-//     }
-//     else if (strncmp(line_pc, "bal_kd:", 6) == 0) {
-//         BALANCE_KD = values_pf[0];
-//     }
-//     else if (strncmp(line_pc, "spd_kp:", 6) == 0) {
-//         SPD_KP = values_pf[0];
-//     }
-//     else if (strncmp(line_pc, "spd_ki:", 6) == 0) {
-//         SPD_KI = values_pf[0];
-//     }
-//     else if (strncmp(line_pc, "tar_spd:", 7) == 0) {
-//         rtU.target_speed = values_pf[0];
-//     }
-//     else if(strncmp(line_pc,"turn_kp:",7) == 0)
-//     {
-//         TURN_KP = values_pf[0];
-//     }
-//     else if(strncmp(line_pc,"turn_kd:",7) == 0)
-//     {
-//         TURN_KD = values_pf[0];
-//     }
-//     else if(strncmp(line_pc,"spd_kd:",7) == 0)
-//     {
-//         SPD_KD  = values_pf[0];
-//     }
-//     else if (strncmp(line_pc, "convert:", 8) == 0) {
-//         rtU.running_flag ^=1;
-//         gpio_pin_set_dt(&led_motor_st, rtU.running_flag);
-//     }
-//     else if (strncmp(line_pc, "turn_tar:", 9) == 0) {
-//         rtU.turn_target = values_pf[0];
-//         rtU.target_speed =values_pf[1];
-//     }
+    if (strncmp(line_pc, "tar_spd:", 8) == 0) {
+        uart_transmit(g_uart_computer, "tar_spd_set_success\r\n", 21);
+    }
+}
 
-// }
+int debug_init(void)
+{
+    return uart_register_callback(g_uart_computer, debug_par_check,NULL);
+}
+
+SYS_INIT(debug_init, APPLICATION, 13);
--- src/debug/debug.h
+++ src/debug/debug.h
@@ -1,10 +1,10 @@
 #ifndef  __DEBUG_H
 #define  __DEBUG_H
 
+#include "stdint.h"
+
 #define ACC_ANGLE 0
 #define SIMULINK_DEBUG 0
-
-void debug_par_check(void);
 
 
 
